7ae4f72193e20c4a4f3070226425196ddf73a8b2
Jay Mahabal <jay.mahabal@akqa.com>
2018-04-25 15:19:11 -0700

    added variablity and ensured bot isnt checking for azalea

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import praw
 import time
 import string 
+import random
 
 SUBREDDIT_NAME = 'warriors'
-RESPONSE = '''
-Hi, you seem to have used "Iggy" to refer to Andre Igoudala, two-time NBA champion and bearer of the best-looking biceps in the NBA. In [several](https://www.youtube.com/watch?v=-qSY3OVrS9E) [instances](https://twitter.com/andre/status/908090035697631232) Andre has said that he would rather be called by a different nickname.
-&nbsp;  
-&nbsp;
-
-*****
-^(If you have complaints or suggestions on how to improve this bot please message myself or the moderators!)
-'''
 
 import credentials
 
@@ -30,10 +23,37 @@ def authenticate():
         username=USERNAME,
         password=PASSWORD)
  
+NBA_DESCRIPTIONS = set([
+    'two-time NBA champion',
+    'NBA Finals MVP',
+    'NBA All-Star',
+])
+
+PERSONAL_DESCRIPTIONS = set([
+    'bearer of the best-looking biceps in the NBA',
+    'noted LeBron stopper',
+])
+
+def generate_response():
+    return '''
+    Hi, you seem to have used "Iggy" to refer to Andre Igoudala, {} and {}. In [several](https://www.youtube.com/watch?v=-qSY3OVrS9E) [instances](https://twitter.com/andre/status/908090035697631232) Andre has said that he would rather be called by a different nickname.
+    &nbsp;  
+    &nbsp;
+
+    *****
+    ^(If you have complaints or suggestions on how to improve this bot please message myself or the moderators!)
+    '''.format(random.sample(NBA_DESCRIPTIONS, 1)[0], random.sample(PERSONAL_DESCRIPTIONS, 1)[0])
+
 def has_iggy(text):
+    text = text.lower()
+    if "azalea" in text:
+        return False
+    if "'iggy'" in text or '"iggy"' in text:
+        return False
+
     # strip punctuation and split on spaces
-    exclude = set(string.punctuation) - set("-")
-    text = ''.join(ch if ch not in exclude else " " for ch in text.lower()).split()
+    exclude = set(string.punctuation) - set("-") # for dont-call-me-iggy
+    text = ''.join(ch if ch not in exclude else " " for ch in text).split()
     
     iggies = []
     for g in range(2, 5):
@@ -63,7 +83,7 @@ def watch_stream():
         if has_iggy(comment.body):
             print ("processing comment #", comment.id)
             comment.save()
-            reply = comment.reply(RESPONSE)
+            reply = comment.reply(generate_response())
             print ("Posted to: https://www.reddit.com/r/" + SUBREDDIT_NAME + "/comments/" + comment.submission.id + "//" + reply.id + "")
 
 def run_tests():
@@ -82,8 +102,15 @@ def run_tests():
     print ("Should all be false:")
     print (has_iggy("andre igoudala"))
     print (has_iggy("getting jiggy with it"))
+    print (has_iggy("dont call him 'iggy'!"))
+    print (has_iggy('dont call him "iggy"!'))
     print (has_iggy("iggy-dala"))
+    print (has_iggy("iggy azalea"))
     print (has_iggy("paging /u/dont-call-me-iggy"))
+
+    print ("Should return 10 different sample responses")
+    for i in range(10):
+        print (generate_response())
 
 if __name__ == '__main__':
     watch_stream()
